b650c0de2689fe97b7cbd4b08a4f5eb47faf60f9
Luka <37179837+luka13499@users.noreply.github.com>
2019-12-03 15:01:22 +0100

    Implementiraj strukturu projekta i kretanje glavnog lika
    Close #4
    Close #3

---
+++ Assets/Scripts/Player Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 8f, maxVelocity = 4f;
+    [SerializeField]
+    private Rigidbody2D myBody;
+    [SerializeField]
+    private Animator anim;
+
+    private void Awake()
+    {
+
+    }
+
+
+    void Start()
+    {
+
+    }
+
+
+    void FixedUpdate()
+    {
+        PlayerMoveKeyboard();
+
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        float forceX = 0f;
+        float vel = Mathf.Abs(myBody.velocity.x);
+
+        float h = Input.GetAxisRaw("Horizontal");
+
+        if (h > 0)
+        {
+            if (vel < maxVelocity)
+            {
+                forceX = speed;
+
+                Vector3 temp = transform.localScale;
+                temp.x = -1.3f;
+                transform.localScale = temp;
+
+                anim.SetBool("Walk", true);
+            }
+        }
+        else if (h < 0)
+        {
+            if (vel < maxVelocity)
+            {
+                forceX = -speed;
+
+                Vector3 temp = transform.localScale;
+                temp.x = 1.3f;
+                transform.localScale = temp;
+
+                anim.SetBool("Walk", true);
+            }
+        }
+        else
+        {
+            anim.SetBool("Walk", false);
+        }
+
+        myBody.AddForce(new Vector2(forceX, 0));
+    }
+}
