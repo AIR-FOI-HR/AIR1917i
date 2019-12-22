2e43e47e5815aa9cf6b90150e404f8ef191700c9
Luka <37179837+luka13499@users.noreply.github.com>
2019-12-22 21:45:33 +0100

    Omogući pozicioniranje igrača, kretanje kamere, generiranje oblaka
    Omogućno je pozicioniranje igrača na početku scene, kretanje kamere u smjeru prema dolje te generiranje novih oblaka
    
    Close #17
    Close #18
    Close  #19

--- Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
+++ Assets/Scripts/Cloud Collectors Scripts/CloudSpawner.cs
@@ -19,18 +19,22 @@ public class CloudSpawner : MonoBehaviour {
     [SerializeField]
     private GameObject[] collectables;
 
+   
     private GameObject player;
-
-    // Use this for initialization
-    void Start () {
-		
-	}
+  
+   
 
     private void Awake()
     {
         controlX = 0;
         SetMinAndMax();
         CreateClouds();
+        player = GameObject.FindGameObjectWithTag("Player");        
+    }
+
+    private void Start()
+    {
+        PositionThePlayer();
     }
 
 
@@ -94,6 +98,96 @@ public class CloudSpawner : MonoBehaviour {
             clouds[i].transform.position = temp;
 
             positionY -= distanceBetweenClouds;
+        }
+    }
+
+    void PositionThePlayer()
+    {
+        GameObject[] darkClouds = GameObject.FindGameObjectsWithTag("Deadly");
+        GameObject[] cloudsInGame = GameObject.FindGameObjectsWithTag("Cloud");
+
+        for (int i = 0; i < darkClouds.Length; i++)
+        {
+            if (darkClouds[i].transform.position.y == 0f)
+            {
+                Vector3 t = darkClouds[i].transform.position;
+
+                darkClouds[i].transform.position = new Vector3(cloudsInGame[0].transform.position.x,
+                                                               cloudsInGame[0].transform.position.y,
+                                                               cloudsInGame[0].transform.position.z);
+
+                cloudsInGame[0].transform.position = t;
+
+
+
+
+            }
+        }
+
+
+        Vector3 temp = cloudsInGame[0].transform.position;
+
+
+        for (int i = 1; i < cloudsInGame.Length; i++)
+        {
+            if (temp.y < cloudsInGame[i].transform.position.y)
+            {
+
+                temp = cloudsInGame[i].transform.position;
+            }
+        }
+
+        temp.y += 0.8f;
+
+        player.transform.position = temp;
+    }
+
+    private void OnTriggerEnter2D(Collider2D target)
+    {
+        if (target.tag == "Cloud" || target.tag == "Deadly")
+        {
+            if (target.transform.position.y == lastCloudPositionY)
+            {
+                GetRandomClouds(clouds);
+                
+
+                Vector3 temp = target.transform.position;
+
+                for (int i = 0; i < clouds.Length; i++)
+                {
+                    if (!clouds[i].activeInHierarchy)
+                    {
+                        if (controlX == 0)
+                        {
+                            temp.x = Random.Range(0.0f, maxX);
+                            controlX = 1;
+                        }
+                        else if (controlX == 1)
+                        {
+                            temp.x = Random.Range(0.0f, minX);
+                            controlX = 2;
+                        }
+                        else if (controlX == 2)
+                        {
+                            temp.x = Random.Range(1.0f, maxX);
+                            controlX = 3;
+                        }
+                        else if (controlX == 3)
+                        {
+                            temp.x = Random.Range(-1.0f, minX);
+                            controlX = 0;
+                        }
+
+                        temp.y -= distanceBetweenClouds;
+
+                        lastCloudPositionY = temp.y;
+
+                        clouds[i].transform.position = temp;
+                        clouds[i].SetActive(true);
+                    }
+                }
+
+            }
         }
     }
 
