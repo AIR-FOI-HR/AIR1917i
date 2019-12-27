db74f50e150f1fc0cf10eead67ba77765fe7930b
Luka <37179837+luka13499@users.noreply.github.com>
2019-12-27 18:20:27 +0100

    Dodaj scenu za "Highscore" te omogući navigaciju između scena
    Dodana je scena za Highscore u kojem će biti prikazan najbolji ostvareni rezultat u igri te je implementirana navigacija između scena
    
    Close #23
    Close #24

---
+++ Assets/Scripts/Game Controllers/MainMenuController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level One");
+
+    }
+
+    public void HighscoreMenu()
+    {
+        SceneManager.LoadScene("Highscore Scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void MusicButton()
+    {
+       
+    }
+}
