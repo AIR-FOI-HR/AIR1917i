af827d411b4f0ca80a27a2d2e9a721a5a477565a
Luka <37179837+luka13499@users.noreply.github.com>
2019-12-31 12:47:52 +0100

    Dodaj skriptu za upravljanje događajima u igri #30
    Dodana je skripta za upravljanje događajima poput pauziranje igre, nastavljanje igre, ažuriranje teksta koji prikazuje broj prikupljenih novčića i slično.
    
    Close #30

--- Assets/Scripts/Player Scripts/PlayerScore.cs
+++ Assets/Scripts/Player Scripts/PlayerScore.cs
@@ -41,6 +41,7 @@ public class PlayerScore : MonoBehaviour {
                 scoreCount++;
             }
             previousPosition = transform.position;
+            GameplayController.instance.SetScore(scoreCount);
         }
     }
 
@@ -50,7 +51,10 @@ public class PlayerScore : MonoBehaviour {
         {
             coinCount++;
             scoreCount += 200;
-            
+
+            GameplayController.instance.SetScore(scoreCount);
+            GameplayController.instance.SetCoinScore(coinCount);
+
             AudioSource.PlayClipAtPoint(coinClip, transform.position);
             target.gameObject.SetActive(false);
         }
@@ -59,7 +63,10 @@ public class PlayerScore : MonoBehaviour {
         {
             lifeCount++;
             scoreCount += 300;
-            
+
+            GameplayController.instance.SetScore(scoreCount);
+            GameplayController.instance.SetLifeScore(lifeCount);
+
             AudioSource.PlayClipAtPoint(lifeClip, transform.position);
             target.gameObject.SetActive(false);
         }
